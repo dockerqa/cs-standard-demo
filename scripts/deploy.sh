548604a8b3ae9d883f2ebf2e68f455d4faba43ae
Lev Lazinskiy <lev@levlaz.org>
2017-03-15 12:31:31 -0700

    Add Procfile

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ssh ubuntu@demo.levops.net 'docker pull levlaz/aws-demo'
-ssh ubuntu@demo.levops.net 'docker stop aws-demo && docker rm aws-demo'
-ssh ubuntu@demo.levops.net 'docker run --name aws-demo -d -p 80:5000 levlaz/aws-demo'
