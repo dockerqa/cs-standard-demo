bdf22b6ea96b3cdc5493b36a116cd672679f5838
Lev Lazinskiy <lev@levlaz.org>
2017-03-15 14:14:36 -0700

    Fix failing test

--- tests/test_flaskr.py
+++ tests/test_flaskr.py
@@ -45,7 +45,7 @@ def logout(client):
 def test_empty_db(client):
     """Start with a blank database."""
     rv = client.get('/')
-    assert b'pproduction' in rv.data
+    assert b'production' in rv.data
 
 
 def test_login_logout(client):
